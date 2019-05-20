254737ce650a0d7625c0f0b761537d9a77eee1eb
Maite Navarro <maitechu87@gmail.com>
2019-05-20 15:52:53 +0200

    Exercici 2

--- src/Lloguer.java
+++ src/Lloguer.java
@@ -1,2 +1,36 @@
+import java.util.Date;
+
 public class Lloguer {
+    private Date data;
+    private int dies;
+    private Vehicle vehicle;
+
+    public Lloguer(Date data, int dies, Vehicle vehicle){
+        this.data = data;
+        this.dies = dies;
+        this.vehicle = vehicle;
+    }
+
+    public Date getData()     { return data;     }
+    public void setData(Date data){ this.data = data;}
+
+    public int getDies()     { return dies;     }
+    public void setDies(int dies){ this.dies = dies;}
+
+    public Vehicle getVehicle(){ return vehicle;}
+    public void setVehicle(Vehicle vehicle){this.vehicle=vehicle;}
+
+    public void llogar(Vehicle vehic){
+        setVehicle(vehic);
+    }
+    public void eliminarLloguer(){setVehicle(null);}
+
+    public boolean estaLlogat() {
+        if (vehicle != null) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
 }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,2 +1,30 @@
+import java.util.Vector;
+
 public class Vehicle {
+    private String model;
+    private String marca;
+    private Tipus categoria;
+
+    public static Tipus BASIC = Tipus.BASIC;
+    public static Tipus GENERAL = Tipus.GENERAL;
+    public static Tipus LUXE = Tipus.LUXE;
+
+    private enum Tipus{ BASIC, GENERAL, LUXE}
+
+
+    public Vehicle(String model, String marca, Tipus categor){
+        this.model = model;
+        this.marca = marca;
+        this.categoria = categor;
+    }
+
+    public String getModel()     { return model;     }
+    public String getMarca()     { return marca;     }
+    public Tipus getCategoria() { return categoria; }
+
+    public void setModel(String model) { this.model = model; }
+    public void setMarca(String marca) { this.marca = marca; }
+    public void setCategoria(Tipus categor) { this.categoria = categor;}
+
+
 }
