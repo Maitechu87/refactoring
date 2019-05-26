112cf0a618d33f972ee3fcb03048c9b4c7bbfc43
Maite Navarro <maitechu87@gmail.com>
2019-05-26 17:47:33 +0200

    Exercici 12 OK bonificacions de Lloguer

--- src/Client.java
+++ src/Client.java
@@ -38,7 +38,6 @@ public class Client {
         return lloguers.contains(lloguer);
     }
 
-
     public String informe() {
         double total = 0;
         int bonificacions = 0;
@@ -46,24 +45,14 @@ public class Client {
                 getNom() +
                 " (" + getNif() + ")\n";
         for (Lloguer lloguer: lloguers) {
-            double quantitat = lloguer.quantitat();
-
-            // afegeix lloguers freqüents
-            bonificacions ++;
-
-            // afegeix bonificació per dos dies de lloguer de Luxe
-            if (lloguer.getVehicle().getCategoria() == Vehicle.LUXE &&
-                    lloguer.getDies()>1 ) {
-                bonificacions ++;
-            }
-
+            bonificacions += bonificacionsDeLloguer(lloguer);
             // composa els resultats d'aquest lloguer
             resultat += "\t" +
                     lloguer.getVehicle().getMarca() +
                     " " +
                     lloguer.getVehicle().getModel() + ": " +
-                    (quantitat * 30) + "€" + "\n";
-            total += quantitat * 30;
+                    (lloguer.quantitat() * 30) + "€" + "\n";
+            total += lloguer.quantitat() * 30;
         }
 
         // afegeix informació final
@@ -72,5 +61,13 @@ public class Client {
         return resultat;
     }
 
+    public int bonificacionsDeLloguer(Lloguer lloguer){
+        if (lloguer.getVehicle().getCategoria() == Vehicle.LUXE &&
+                lloguer.getDies()>1 ) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
 
 }
